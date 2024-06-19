846c12eac5f51d84ffa54ecd22d62d2fe8f65687
Philip Corliss <pcorliss@50projects.com>
2024-06-18 23:12:19 -0500

    Added cape - Animation is a little wonky but it's rendering properly.

--- src/main.ino
+++ src/main.ino
@@ -4,6 +4,7 @@
 
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 TFT_eSprite mario = TFT_eSprite(&tft);
+TFT_eSprite cape = TFT_eSprite(&tft);
 TFT_eSprite background = TFT_eSprite(&tft);
 
 void setup(void)
@@ -29,22 +30,28 @@ void setup(void)
 
   Serial.println("Initialized");
   mario.createSprite(16, 32);
+  cape.createSprite(16, 16);
 }
 
 void loop(void)
 {
-  for (int x = 0, mario_idx = 0; x < TFT_HEIGHT - 16; x++, mario_idx++)
+  for (int x = 0, mario_idx = 0, cape_idx = 0; x < TFT_HEIGHT - 16; x++, mario_idx++, cape_idx++)
   {
     background.fillSprite(TFT_OLIVE);
-    mario.pushImage(0, 0, 16, 32, (uint16_t *)epd_bitmap_allArray[mario_idx % 3]);
+    mario_idx %= epd_bitmap_mario_LEN;
+    cape_idx %= epd_bitmap_cape_LEN;
+    mario.pushImage(0, 0, 16, 32, (uint16_t *)epd_bitmap_mario[mario_idx]);
+    cape.pushImage(0, 0, 16, 16, (uint16_t *)epd_bitmap_cape[cape_idx]);
     // The transparency masked is swapped 0x03ae -> 0xae03.
     // Not quite sure why. Probably swapping the bytes.
     // & is automatic because of the overflow in C++
     // (mario << 8 | mario >> 8) & (2**16 -1)
+    // Cape offset by 10x10
+    cape.pushToSprite(&background, x - 10, 0 + 10, 0xae03);
     mario.pushToSprite(&background, x, 0, 0xae03);
 
     background.pushSprite(0, 0);
 
-    delay(100);
+    delay(500);
   }
 }
